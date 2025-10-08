5c031083c8e75308c69c23796b79569504152e65
KESHAV KUMAR JHA <112645121+keshavjha005@users.noreply.github.com>
2025-10-08 18:33:08 +0530

    doctors page changed

--- src/page-components/doctors/DoctorsInfo.tsx
+++ src/page-components/doctors/DoctorsInfo.tsx
@@ -21,7 +21,7 @@ export const doctors: Doctor[] = [
     slug: "dr-narhari-s-malgaonkar",
     name: "Dr. Narhari S. Malgaonkar",
     bio:
-      "Dr. Narhari S. Malgaonkar is the dedicated and exclusive chief fertility consultant.",
+      "Dr. Narhari S. Malagaonkar is the dedicated and exclusive chief fertility consultant. Dr. Narhari S. Malagaonkar is the dedicated and exclusive chief fertility consultant.",
     experience: "12+ Years of Experience",
     qualifications: "DNB/DGO (Obstetrics and Gynaecology)",
     fellowship: "Fellowship in Reproductive Medicine",
@@ -34,7 +34,7 @@ export const doctors: Doctor[] = [
     slug: "dr-sonali-malgaonkar",
     name: "Dr. Sonali Malgaonkar",
     bio:
-      "Dr. Sonali Malgaonkar is the dedicated and exclusive chief fertility consultant.",
+      "Dr. Sonali Malagaonkar is the dedicated and exclusive chief fertility consultant. Dr. Sonali Malagaonkar is the dedicated and exclusive chief fertility consultant.",
     experience: "12+ Years of Experience",
     qualifications: "DNB/DGO (Obstetrics and Gynaecology)",
     fellowship: "Fellowship in Reproductive Medicine",
@@ -46,7 +46,7 @@ export const doctors: Doctor[] = [
    {
     slug: "dr-unnati-mamtora",
     name: "Dr. Unnati Mamtora",
-    bio: "Dr. Unnati Mamtora is a dedicated Fertility Consultant at Borivali.",
+    bio: "Fertility Consultant, Borivali",
     experience: "12+ Years of Experience",
     qualifications: "DNB DGO (Obstetrics and Gynaecology)",
     fellowship: "Fellowship in Reproductive Medicine",
@@ -58,7 +58,7 @@ export const doctors: Doctor[] = [
 {
     slug: "dr-dinesh-wade",
     name: "Dr. Dinesh Wade",
-    bio: "Dr. Dinesh Wade is a dedicated Fertility Consultant at Pune.",
+    bio: "Fertility Consultant, Pune",
     experience: "14+ Years of Experience",
     qualifications: "MBBS, MS, DNB (OBST & GYNAE), FNB, MRCOG (UK)",
     fellowship: "Fellowship in Reproductive Medicine",
@@ -70,7 +70,7 @@ export const doctors: Doctor[] = [
 {
     slug: "dr-shital-sonone",
     name: "Dr. Shital Sonone",
-    bio: "Dr. Shital Sonone is a dedicated Fertility Consultant at Nashik.",
+    bio: "Fertility Consultant, Nashik.",
     experience: "09+ Years of Experience",
     qualifications: "MS (Obstetrics and Gynaecology)",
     fellowship: "Fellowship in Reproductive Medicine",
@@ -82,7 +82,7 @@ export const doctors: Doctor[] = [
 {
     slug: "dr-rajashree-patil",
     name: "Dr. Rajashree Patil",
-    bio: "Dr. Rajashree Patil is a dedicated Fertility Consultant at Virar.",
+    bio: "Fertility Consultant, Virar.",
     experience: "12+ Years of Experience",
     qualifications: "DNB DGO (Obstetrics and Gynaecology)",
     fellowship: "Fellowship in Reproductive Medicine",
@@ -94,7 +94,7 @@ export const doctors: Doctor[] = [
 {
     slug: "dr-darshna-wahane",
     name: "Dr. Darshna Wahane",
-    bio: "Dr. Darshna Wahane is a dedicated Fertility Consultant at Panvel.",
+    bio: "Fertility Consultant, Panvel",
     experience: "10+ Years of Experience",
     qualifications: "MBBS DGO (OBST & GYNAE)",
     fellowship: "Fellowship in Reproductive Medicine",
@@ -106,7 +106,7 @@ export const doctors: Doctor[] = [
 {
     slug: "dr-priti-pardeshi",
     name: "Dr. Priti Pardeshi",
-    bio: "Dr. Priti Pardeshi is a dedicated Fertility Consultant at Kalyan.",
+    bio: "Fertility Consultant, Kalyan",
     experience: "12+ Years of Experience",
     qualifications: "DGO (Obstetrics and Gynaecology)",
     fellowship: "Fellowship in Reproductive Medicine",
@@ -118,7 +118,7 @@ export const doctors: Doctor[] = [
 {
     slug: "dr-teena-desai",
     name: "Dr. Teena Desai",
-    bio: "Dr. Teena Desai is a dedicated Fertility Consultant at Andheri.",
+    bio: "Fertility Consultant, Andheri.",
     experience: "10+ Years of Experience",
     qualifications: "DNB DGO (Obstetrics and Gynaecology)",
     fellowship: "Fellowship in Reproductive Medicine",
@@ -130,7 +130,7 @@ export const doctors: Doctor[] = [
 {
     slug: "dr-bhavika-sane",
     name: "Dr. Bhavika Sane",
-    bio: "Dr. Bhavika Sane is a dedicated Fertility Consultant at Vashi.",
+    bio: "Fertility Consultant, Vashi.",
     experience: "04+ Years of Experience",
     qualifications: "DGO (Obstetrics and Gynaecology)",
     fellowship: "Fellowship in Reproductive Medicine",
@@ -142,7 +142,7 @@ export const doctors: Doctor[] = [
 {
     slug: "dr-prerna-khandelwal",
     name: "Dr. Prerna Khandelwal",
-    bio: "Dr. Prerna Khandelwal is a dedicated Fertility Consultant at Thane.",
+    bio: "Fertility Consultant, Thane.",
     experience: "04+ Years of Experience",
     qualifications: "MS (Obstetrics and Gynaecology)",
     fellowship: "Fellowship in Reproductive Medicine",
@@ -154,7 +154,7 @@ export const doctors: Doctor[] = [
 {
     slug: "dr-shradha-pol",
     name: "Dr. Shradha Pol",
-    bio: "Dr. Shradha Pol is a dedicated Fertility Consultant at Thane.",
+    bio: "Fertility Consultant, Thane.",
     experience: "04+ Years of Experience",
     qualifications: "MS (Obstetrics and Gynaecology)",
     fellowship: "Fellowship in Reproductive Medicine",
@@ -166,7 +166,7 @@ export const doctors: Doctor[] = [
 {
     slug: "dr-vivek-bagul",
     name: "Dr. Vivek Bagul",
-    bio: "Dr. Vivek Bagul is a dedicated Fertility Consultant at Jalgaon.",
+    bio: "Fertility Consultant, Jalgaon.",
     experience: "04+ Years of Experience",
     qualifications: "DGO (Obstetrics and Gynaecology)",
     fellowship: "Fellowship in Reproductive Medicine",
@@ -182,70 +182,73 @@ const DoctorsInfo: React.FC = () => {
   const pathname = usePathname();
   const locale = pathname?.split("/")[1] || "en";
   return (
-    <section className="section-spacing w-full bg-white px-6 md:px-12 lg:px-[90px] py-10 md:py-14">
+    <section className="section-spacing w-full bg-white px-6 md:px-12  lg:px-[90px] py-10 md:py-14">
       {/* Top breadcrumb tag */}
-      <span className="inline-block bg-[#1656A50D] text-[#1656A5] text-[12px] md:text-[13px] px-3 py-1 rounded-[8px]" >Our Doctors</span>
-      <h2 className="text-[32px] gap-[16px] md:text-[40px] font-semibold leading-tight text-[#2C2C2C] mb-2 font-[Manrope]" >
+      <span className="inline-block bg-[#1656A50D] text-[#1656A5] lg:gap-[8px] text-[12px] md:text-[13px] px-3 py-1 rounded-[8px]" >Our Doctors</span>
+      <h2 className="text-[32px] gap-[46px] md:text-[40px] font-semibold leading-tight text-[#2C2C2C] mb-2 font-[Manrope]" >
         Meet our Experts
       </h2>
 
       <div className="pb-4">
         {doctors.map((d, idx) => (
-          <div key={idx} className="w-full border-b  border-gray-200 py-8 md:py-10 mb-8 md:mb-10 last:border-b-0 last:mb-0">
-            <div onClick={() => router.push(`/doctors/${d.slug}`)} className="grid  grid-cols-1 lg:grid-cols-[320px_1fr_260px] gap-6 items-start">
+         <div
+
+  key={idx}
+  className="w-full border-b border-gray-200 py-3 md:py-4 mb-2 md:mb-3 last:border-b-0 last:mb-0"
+>
+
+
+            <div onClick={() => router.push(`/doctors/${d.slug}`)} className="grid  grid-cols-1 lg:grid-cols-[320px_1fr_260px] gap-6 items-start hover:bg-[#1656A50D] lg:p-4 rounded-[16px] cursor-pointer">
               {/* Left: Image */}
               <div className="w-full h-full md:h-full lg:h-full overflow-hidden rounded-[16px] bg-gray-100">
                 <img
                   src={d.image}
                   alt={d.name}
-                  className="w-full h-fit object-cover"
+                  className="w-full h-full object-cover"
                 />
               </div>
 
               {/* Middle: Content */}
               <div className="font-[Manrope]" >
-                <h3 className="text-[26px] md:text-[32px] text-[#1656A5] font-semibold tracking-[-0.02em]">
+                <h3 className="text-[26px] md:text-[32px] text-[#1656A5] gap-[8px] font-semibold tracking-[-0.02em]">
                   {d.name}
                 </h3>
-                <p className="mt-2 text-[14px] md:text-[15px] text-[#6B7280] max-w-[780px]">
+                <p className="mt-2 text-[14px] md:text-[15px] text-[#6B7280]  max-w-[780px]">
                   {d.bio}
                 </p>
 
-                <div className="mt-3 flex flex-wrap gap-x-4 gap-y-2 text-[13px] md:text-[14px] font-medium text-[#1656A5]">
-                  <a className="hover:underline" href="#">{d.experience}</a>
-                  <span className="text-gray-300">|</span>
-                  <a className="hover:underline" href="#">{d.qualifications}</a>
-                  <span className="text-gray-300">|</span>
-                  <a className="hover:underline" href="#">{d.fellowship}</a>
-                </div>
-              </div>
-
-              {/* Right: Meta row (Hospital + Specialty) + CTA */}
-              <div className="flex flex-col items-start lg:items-end gap-6 font-[Manrope]">
-                {/* Row: Hospital | Specialty */}
-                <div className="w-full flex xsm:flex-row md:flex-col justify-between gap-6 md:gap-10 lg:justify-end">
-                  <div className="min-w-[110px] lg:text-right">
-                    <div className="text-[16px] lg:text-[32px] text-[#1656A5]">Hospital</div>
-                    <div className="text-[16px] text-[#2C2C2C] font-medium">{d.hospital}</div>
-                  </div>
-                  <div className="min-w-[140px] lg:text-right">
-                    <div className="text-[16px] lg:text-[32px] text-[#1656A5]">Specialty</div>
-                    <div className="text-[16px] text-[#2C2C2C] font-medium">
-                      {d.specialty}
-                    </div>
-                  </div>
-                </div>
-
-                {/* CTA */}
-                <button
-                  
-                  className="h-[44px] px-5 rounded-[16px] bg-[#252525] text-white text-sm font-semibold shadow-sm hover:bg-[#000000] p-2"
-                >
-                  Book Your Appointment
-                </button>
+              <div className="mt-[26px] flex flex-col gap-[8px] text-[13px] md:text-[14px]  font-medium text-[#1656A5]">
+                <a className="hover:underline" href="#">{d.experience}</a>
+                <a className="hover:underline" href="#">{d.qualifications}</a>
+                <a className="hover:underline" href="#">{d.fellowship}</a>
               </div>
             </div>
+
+            {/* Right: Meta row (Hospital + Specialty) + CTA */}
+            <div className="flex flex-col items-start lg:items-end gap-4 lg:gap-[90px] font-[Manrope]">
+              {/* Row: Hospital | Specialty */}
+              <div className="w-full flex xsm:flex-row md:flex-col justify-between  lg:justify lg:gap-[24px]">
+                <div className="min-w-[188px] lg:gap-[8px] lg:text-right">
+                  <div className="text-[16px] lg:text-[32px] text-[#1656A5]">Hospital</div>
+                  <div className="text-[16px] text-[#2C2C2C] font-medium">{d.hospital}</div>
+                </div>
+                <div className="min-w-[188px] lg:gap-[8px] lg:text-right">
+                  <div className="text-[16px] lg:text-[32px] text-[#1656A5]">Specialty</div>
+                  <div className="text-[16px]   text-[#2C2C2C] font-medium">
+                    {d.specialty}
+                  </div>
+                </div>
+              </div>
+
+              {/* CTA */}
+              <button
+                className="h-[56px]  px-5 rounded-[16px] bg-[#252525] text-white text-sm font-semibold shadow-sm hover:bg-[#000000] p-2"
+              >
+                Book Your Appointment
+              </button>
+            </div>
           </div>
+        </div>
         ))}
       </div>
     </section>
--- src/page-components/doctors/DoctorsPart.tsx
+++ src/page-components/doctors/DoctorsPart.tsx
@@ -3,19 +3,37 @@ import React from "react";
 const DoctorsPart = () => {
   return (
     <section>
-    <div className="section-spacing relative flex w-full h-[320px] bg-gradient-to-r from-green-100 via-white to-sky-200 px-6 md:px-12 lg:px-[90px] py-10 md:py-14">
-      
-      {/* Bottom-left paragraph */}
-      <p className="absolute bottom-36 lg:bottom-0 lg:left-0  max-w-[500px] px-4 pb-4 text-[16px] text-center lg:text-start md:text-[18px] text-[#606060]">
-        Connect with the specialists who combine world-class expertise with
-        genuine care, and take your first step toward parenthood.
-      </p>
+      <div
+        className="section-spacing relative flex w-full h-[605px] px-10 md:px-12 lg:px-[90px] py-10 md:py-14 bg-cover bg-center"
+        style={{
+          backgroundImage: "url('/DoctorsSection/Doctors banner mobile.png')",
+        }}
+      >
+        {/* Large screen background */}
+        <div
+          className="hidden lg:block absolute inset-0 bg-cover bg-center"
+          style={{
+            backgroundImage: "url('/DoctorsSection/Doctors banner 2.png')",
+          }}
+        ></div>
 
-      {/* Top-right heading */}
-      <h2 className="absolute top-0 right-0 text-[#94BA3D] lg:text-[#1656A5] text-center lg:text-start lg:w-[640px] font-semibold text-[32px] lg:text-[46px] leading-[97%] tracking-[-0.02em] px-4 pt-4">
-        Book your appointment with our experts today.
-      </h2>
-    </div>
+        {/* Overlay for better text contrast */}
+        <div className="absolute inset-0 bg-black opacity-20 lg:opacity-0"></div>
+
+        {/* Top-right heading */}
+        <h2 className="absolute top-[60px] right-[40px] lg:right-[106px] text-[#1656A5] text-right font-semibold text-[28px] md:text-[40px] leading-[1.2] tracking-[-0.02em] z-10">
+          <span className="block">Book your appointment</span>
+          <span className="block">with our experts today.</span>
+        </h2>
+
+        {/* Paragraph text */}
+        <p className="absolute text-[14px] md:text-[16px] text-[#3B3B3B] text-center leading-[1.4] z-10
+          w-full px-6 top-[180px] lg:top-auto lg:bottom-10 lg:left-[90px] lg:right-auto lg:text-start lg:w-auto"
+        >
+          Connect with the specialists who combine world-class expertise with
+          genuine care, and take your first step toward parenthood.
+        </p>
+      </div>
     </section>
   );
 };
