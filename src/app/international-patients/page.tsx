a064a9e5f8106d4944b40b2c2ced07ea6ebe150f
zgourav <gourav.bhandari@zucolservices.com>
2025-10-09 17:13:11 +0530

    updated

--- src/app/international-patients/page.tsx
+++ src/app/international-patients/page.tsx
@@ -164,7 +164,7 @@ const InternationalPatientsPage: React.FC = () => {
         id="testimonial"
         className="w-full bg-[#1656A5] text-white py-[82px] h-[700px]"
       >
-        <div className="flex items-center mx-auto px-6 md:px-[120px]">
+        <div className="flex items-center gap-[2rem] mx-auto px-6 md:px-[120px]">
           <div className="flex flex-col justify-between w-[350px]">
             <h2
               className="text-[#F9F9F9] font-[Manrope] 
@@ -242,7 +242,7 @@ const InternationalPatientsPage: React.FC = () => {
             <img
               src="/InternationalPatients/testimonial.png"
               alt="Happy Family"
-              className=" rounded-xl h-[536px] shadow-lg"
+              className=" rounded-xl h-auto shadow-lg"
             />
           </div>
         </div>
@@ -272,7 +272,7 @@ const InternationalPatientsPage: React.FC = () => {
               key={cat.id}
               type="button"
               onClick={() => handleScroll(cat.id)}
-              className={`px-[10px] py-[10px] md:px-[20px] md:py-[16px] rounded-[8px] md:rounded-[16px] font-[Manrope] text-[12px] md:text-[14px] font-medium leading-[24px] tracking-[-0.28px] transition-all duration-200 ${
+              className={`px-[10px] cursor-pointer py-[10px] md:px-[20px] md:py-[16px] rounded-[8px] md:rounded-[16px] font-[Manrope] text-[12px] md:text-[14px] font-medium leading-[24px] tracking-[-0.28px] transition-all duration-200 ${
                 activeTab === cat.id
                   ? "bg-[#1656A5] text-white shadow-md"
                   : "border border-[#1656A5] text-[#1656A5] hover:bg-[#1656A5]/10"
