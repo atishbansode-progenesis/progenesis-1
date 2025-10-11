af824cb2f454d5d44542384dfd26a01d7660011c
Mulkesh-sharma <mulkeshsharma2004@gmail.com>
2025-10-11 16:21:22 +0530

    about us page

--- src/page-components/about/Impact.tsx
+++ src/page-components/about/Impact.tsx
@@ -31,7 +31,7 @@ const Impact: React.FC = () => {
       <div className="max-w-5xl md:max-w-3xl lg:max-w-5xl ">
         <span className="inline-block bg-[#1656A50D] text-[#1656A5] text-[12px] md:text-[13px] px-2 py-1 rounded-[8px]" >Impact & Growth</span>
         <div className="hidden md:block">
-        <h2 className="csLg:text-[48px] lg:leading-[56px]leading-[40px] text-[32px] tracking-tight text-[#2C2C2C] font-normal pt-2 md:mb-5 lg:mb-10">
+        <h2 className="csLg:text-[48px] lg:leading-[56px] leading-[40px] text-[32px] tracking-tight text-[#2C2C2C] font-normal pt-2 md:mb-5 lg:mb-10">
           Extending care through
           <br />
           community initiatives while
@@ -40,7 +40,7 @@ const Impact: React.FC = () => {
         </h2>
         </div>
         <div className="block md:hidden">
-        <h2 className="csLg:text-[48px] lg:leading-[56px]leading-[40px] text-[32px] tracking-tight text-[#2C2C2C] font-normal pt-2 md:mb-5 lg:mb-10">
+        <h2 className="csLg:text-[48px] lg:leading-[56px] leading-[40px] text-[32px] tracking-tight text-[#2C2C2C] font-normal pt-2 md:mb-5 lg:mb-10">
           Extending care through
         
           community initiatives while
