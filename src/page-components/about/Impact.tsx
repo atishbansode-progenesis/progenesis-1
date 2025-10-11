1bc2caa09612f107c249b85bd34130116bd65d83
Mulkesh-sharma <mulkeshsharma2004@gmail.com>
2025-10-11 16:14:05 +0530

    about us page

--- src/page-components/about/Impact.tsx
+++ src/page-components/about/Impact.tsx
@@ -28,21 +28,32 @@ const Impact: React.FC = () => {
   return (
     <section id="impact-growth" className="w-full font-[Manrope] bg-[#F5FAFF] section-spacing" >
       {/* Label & Heading */}
-      <div className="max-w-5xl md:max-w-3xl lg:max-w-5xl">
+      <div className="max-w-5xl md:max-w-3xl lg:max-w-5xl ">
         <span className="inline-block bg-[#1656A50D] text-[#1656A5] text-[12px] md:text-[13px] px-2 py-1 rounded-[8px]" >Impact & Growth</span>
-        <h2 className="csLg:text-[48px] lg:leading-[56px] text-[32px] tracking-[0] text-[#2C2C2C] font-normal mb-10">
+        <div className="hidden md:block">
+        <h2 className="csLg:text-[48px] lg:leading-[56px]leading-[40px] text-[32px] tracking-tight text-[#2C2C2C] font-normal pt-2 md:mb-5 lg:mb-10">
           Extending care through
           <br />
           community initiatives while
           <br />
           building a strong future together.
         </h2>
+        </div>
+        <div className="block md:hidden">
+        <h2 className="csLg:text-[48px] lg:leading-[56px]leading-[40px] text-[32px] tracking-tight text-[#2C2C2C] font-normal pt-2 md:mb-5 lg:mb-10">
+          Extending care through
+        
+          community initiatives while
+          
+          building a strong future together.
+        </h2>
+        </div>
       </div>
 
       {/* Cards */}
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5 md:gap-6 pt-10">
         {items.map((it, idx) => (
-          <div key={idx} className="bg-[#EEF5FF] backdrop-blur-sm rounded-2xl p-6 min-h-[220px] flex flex-col">
+          <div key={idx} className="bg-[#EEF5FF] backdrop-blur-sm rounded-2xl p-4 lg:py-[30px] lg:px-[20px] min-h-[180px] md:min-h-[220px] flex flex-col">
             <div className="text-[#1656A5] text-2xl mb-6"><img src={it.icon} alt={it.title} className="w-[30px] h-[30px] object-contain" /></div>
             <h3 className="text-[20px] md:text-[26px] font-normal text-[#2C2C2C] mb-2">{it.title}</h3>
             <p className="mt-1 text-[14px] leading-[22px] text-[#606060]">{it.desc}</p>
