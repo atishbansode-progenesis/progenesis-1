304a1fe10048c78b261e66929053a170723f8bd4
Vivek kumar Singh <109888242+er-viveksingh-cb@users.noreply.github.com>
2025-10-14 12:58:44 +0530

    updates

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -57,20 +57,20 @@ export function SearchSection({ onClose }: { onClose: () => void }) {
   ];
 
   const locations = [
-    { name: "Andheri", desc: "(Opp. to Laxmi Industrial Complex)", icon: "/LocationsSection/mumbai.png", link: "centers/andheri/" },
+    { name: "Andheri", desc: "(Opp. to Laxmi Industrial Complex)", icon: "/LocationsSection/Andheri.jpg", link: "centers/andheri/" },
     { name: "Thane", desc: "(Opp R Mall, Manpada)", icon: "/LocationsSection/thane.png", link: "/centers/thane/" },
-    { name: "Borivali", desc: "(Near Station)", icon: "/LocationsSection/thane.png", link: "/centers/borivali/" },
-    { name: "Ghatkopar", desc: "(Mahatma Gandhi Road)", icon: "/LocationsSection/thane.png", link: "/centers/ghatkopar/" },
-    { name: "Vashi", desc: "(Palm Beach Service Road)", icon: "/LocationsSection/thane.png", link: "/centers/vashi/" },
-    { name: "Virar", desc: "(Virar Medical Complex)", icon: "/LocationsSection/thane.png", link: "/centers/virar/" },
-    { name: "Kalyan", desc: "(Kalyan Health Clinic)", icon: "/LocationsSection/thane.png", link: "/centers/kalyan/" },
-    { name: "Panvel", desc: "(Panvel Medical Center)", icon: "/LocationsSection/thane.png", link: "/centers/panvel/" },
-    { name: "Pune", desc: "(Near FC Road)", icon: "/LocationsSection/pune.png",link: "/centers/pune/" },
-    { name: "Nashik", desc: "(Yeolekar Mala)", icon: "/LocationsSection/nasik.png",link: "/centers/nashik/" },
-    { name: "Jalgaon", desc: "(College Rd)", icon: "/LocationsSection/jalgaon.png",link: "/centers/jalgaon/" },
-    { name: "Solapur", desc: "(Dufferin Chowk)", icon: "/LocationsSection/solapur.png",link: "/centers/solapur/" },
+    { name: "Borivali", desc: "(Near Station)", icon: "/LocationsSection/borivali.png", link: "/centers/borivali/" },
+    { name: "Ghatkopar", desc: "(Mahatma Gandhi Road)", icon: "/LocationsSection/ghatkopar.jpg", link: "/centers/ghatkopar/" },
+    { name: "Vashi", desc: "(Palm Beach Service Road)", icon: "/LocationsSection/vashi.jpg", link: "/centers/vashi/" },
+    { name: "Virar", desc: "(Virar Medical Complex)", icon: "/LocationsSection/virar.jpg", link: "/centers/virar/" },
+    { name: "Kalyan", desc: "(Kalyan Health Clinic)", icon: "/LocationsSection/kalyan.jpg", link: "/centers/kalyan/" },
+    { name: "Panvel", desc: "(Panvel Medical Center)", icon: "/LocationsSection/panvel.jpg", link: "/centers/panvel/" },
+    { name: "Pune", desc: "(Near FC Road)", icon: "/LocationsSection/pune.png", link: "/centers/pune/" },
+    { name: "Nashik", desc: "(Yeolekar Mala)", icon: "/LocationsSection/nasik.png", link: "/centers/nashik/" },
+    { name: "Jalgaon", desc: "(College Rd)", icon: "/LocationsSection/jalgaon.png", link: "/centers/jalgaon/" },
+    { name: "Solapur", desc: "(Dufferin Chowk)", icon: "/LocationsSection/solapur.png", link: "/centers/solapur/" },
     { name: "Nagpur", desc: "(Dharampeth)", icon: "/LocationsSection/nagpur.png", link: "/centers/nagpur/" },
-    { name: "Kolhapur", desc: "(Railway Colony)", icon: "/LocationsSection/kolhapur.png",link: "/centers/kolhapur/" },
+    { name: "Kolhapur", desc: "(Railway Colony)", icon: "/LocationsSection/kolhapur.png", link: "/centers/kolhapur/" },
     { name: "Amravati", desc: "(Sharda Vihar)", icon: "/LocationsSection/amravati.png", link: "/centers/amravati/" },
     { name: "Ahilyanagar", desc: "(Mauli Sankul Road)", icon: "/LocationsSection/ahilyanagar.png", link: "/centers/ahilyanagar/" },
   ];
@@ -254,13 +254,13 @@ export function SearchSection({ onClose }: { onClose: () => void }) {
                   borderRadius: "1rem",
                 }}
               >
-          
+
 
                 {filteredTreatments.map((t, i) => (
                   <button
                     key={i}
                     onMouseDown={(e) => {
-                      e.preventDefault(); 
+                      e.preventDefault();
                       e.stopPropagation();
                       setInputs((prev) => ({ ...prev, what: t.name }));
                       setActiveStep(null);
@@ -574,12 +574,12 @@ export function SearchSection({ onClose }: { onClose: () => void }) {
                 >
                   <div className="w-[48px] h-[48px] overflow-hidden rounded-[12px] bg-gray-100 flex-shrink-0">
                     <Image
-                    src={d.img}
-                    alt={d.name}
-                    width={48}
-                    height={48}
-                    className="rounded-[12px] object-cover"
-                  />
+                      src={d.img}
+                      alt={d.name}
+                      width={48}
+                      height={48}
+                      className="rounded-[12px] object-cover"
+                    />
                   </div>
                   <div>
                     <p className="font-medium text-gray-900">{d.name}</p>
