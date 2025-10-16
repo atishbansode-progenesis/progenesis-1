237f3567b0ee83a1d8422cdf1c4a86f927364320
Gaurav <gauravjharbade2006@gmail.com>
2025-10-16 13:38:47 +0530

    our center

--- src/page-components/centers/CentersLanding.tsx
+++ src/page-components/centers/CentersLanding.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import Image from "next/image";
-import '../about/AboutMain.css';
-// C:\Users\vaibh\OneDrive\Desktop\prog\progenesis\public\images\center-banner-mob.png
-// C:\Users\vaibh\OneDrive\Desktop\prog\progenesis\public\images\center-banner-web.png
+import "../about/AboutMain.css";
+
 const CentersLanding = () => {
   return (
     <section id="landing" className="relative w-screen h-[60vh] overflow-hidden">
@@ -30,11 +29,19 @@ const CentersLanding = () => {
 
       {/* Content */}
       <div className="relative z-10 grid grid-cols-12 gap-4 min-h-[300px] md:min-h-[420px] lg:min-h-[500px] section-spacing">
-        <div className="col-span-12 csLg:col-span-9 flex items-end lg:items-start justify-start min-h-[300px] lg:min-h-auto">
-            <h1 className="font-manrope font-semibold text-[32px] lg:text-[60px] w-[288px] lg:w-auto h-[80px] 
-                      leading-[48px] md:leading-[56px] lg:leading-[88px] tracking-[-0.02em] text-[#2C2C2C]">
-              Our Fertility Centers
-            </h1>
+        <div
+          className="col-span-12 csLg:col-span-9 flex items-center justify-start 
+                     min-h-[300px] lg:min-h-auto 
+                     md:items-center 
+                     items-end pb-6 md:pb-0"
+        >
+          <h1
+            className="font-manrope font-semibold text-[32px] lg:text-[60px] 
+                       w-[288px] lg:w-auto h-[80px] leading-[48px] md:leading-[56px] 
+                       lg:leading-[88px] tracking-[-0.02em] text-[#2C2C2C] text-left"
+          >
+            Our Fertility Centers
+          </h1>
         </div>
       </div>
     </section>
